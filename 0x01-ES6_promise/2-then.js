369c5bbcf7d4ca102c16edbfbe0243da540992d1
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-03-15 22:37:09 +0000

    completed task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,8 @@
 function handleResponseFromAPI(promise) {
-    return promise
-        .then(() => {
-            return { status: 200, body: 'success' };
-        })
-        .catch(() => {
-            return new Error();
-        })
-        .finally(() => {
-            console.log('Got a response from the API');
-        });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
 
 export default handleResponseFromAPI;
-
