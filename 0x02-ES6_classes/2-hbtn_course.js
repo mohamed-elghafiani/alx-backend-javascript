d61bf234c546489b6b0db951b920b4bcee95b18b
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-03-20 22:17:51 +0000

    Getters, and Setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(value) {
+    if (typeof value === 'number') {
+      this._length = value;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(value) {
+    if (Array.isArray(value) && value.every((item) => typeof item === 'string')) {
+      this._students = value;
+    } else {
+      throw new TypeError('Students must be an array of Strings');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+}
