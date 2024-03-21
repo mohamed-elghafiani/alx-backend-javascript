e0d705454f53d3fcf6b9df0176574ffb1c88dc84
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-03-21 00:33:35 +0000

    abstract class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
   constructor(sqft) {
     this.sqft = sqft;
-    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must define evacuationWarningMessage');
     }
   }
