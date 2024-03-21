8896cb4573f26b755b128dbfe63685d39fe6f811
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-03-21 01:22:30 +0000

    fixed eslint issue

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,7 +2,7 @@ export default class Building {
   constructor(sqft) {
     this.sqft = sqft;
     if (this.constructor !== Building) {
-      if(typeof this.evacuationWarningMessage !== 'function') {
+      if (typeof this.evacuationWarningMessage !== 'function') {
         throw new Error('Class extending Building must define evacuationWarningMessage');
       }
     }
