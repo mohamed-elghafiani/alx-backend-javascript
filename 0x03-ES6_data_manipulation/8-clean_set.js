8f3a8afc1175541af433cafeae54640ccdf88614
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-03-22 15:14:28 +0000

    Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string' || !set) {
+    return '';
+  }
+
+  const result = [];
+  for (const elem of set) {
+    if (elem && elem.startsWith(startString) && typeof elem === 'string') {
+      const value = elem.slice(startString.length);
+
+      if (value && value !== elem) result.push(value);
+    }
+  }
+  return result.join('-');
+}
