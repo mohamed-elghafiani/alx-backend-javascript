92578ef67c46e8ee73a3cc602362da0c11e91cc3
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-05-02 10:46:14 +0000

    a small HTTP server using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
+
 const app = express();
+const PORT = 1245;
 
 app.get('/', (_, res) => {
     res.send('Hello Holberton School!');
 });
 
-const PORT = 1245;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
