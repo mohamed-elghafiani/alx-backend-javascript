3956943b072ae6e3ee304ea10534e8605727d250
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-05-02 11:23:15 +0000

    a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -6,29 +6,31 @@ const app = express();
 const PORT = 1245;
 
 app.use((req, res, next) => {
-    const dbFilePath = process.argv[2];
-    if (!dbFilePath) {
-        return res.status(500).send('No database file specified');
-    }
-    next();
+  const dbFilePath = process.argv[2];
+  if (!dbFilePath) {
+    return res.status(500).send('No database file specified');
+  }
+  next();
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.setHeader('Content-Length', responseText.length);
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
-    const dbFilePath = process.argv[2];
-    try {
-        const result = await countStudents(dbFilePath);
-        res.status(200).send(`This is the list of our students\n${result}`);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+  const dbFilePath = process.argv[2];
+  res.setHeader('Content-Type', 'text/plain');
+  try {
+    const result = await countStudents(dbFilePath);
+    res.status(200).send(`This is the list of our students\n${result}`);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
 
 module.exports = app;
