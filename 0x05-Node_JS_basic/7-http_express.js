12684097b103770aae8b9a093cdc17b7634b8bbd
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-09-21 03:36:02 -0700

    updated fixed issues

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,34 +1,63 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
-const path = require('path');
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+      if (err) return reject(Error('Cannot load the database'));
+      // split data and taking only list without header
+      const lines = data.split('\n').slice(1, -1);
+      // give the header of data
+      const header = data.split('\n').slice(0, 1)[0].split(',');
+      // find firstname and field index
+      const idxFn = header.findIndex((ele) => ele === 'firstname');
+      const idxFd = header.findIndex((ele) => ele === 'field');
+      // declarate two dictionaries for count each fields and store list of students
+      const fields = {};
+      const students = {};
+      // it will contain all data
+      const all = {};
+
+      lines.forEach((line) => {
+        const list = line.split(',');
+        if (!fields[list[idxFd]]) fields[list[idxFd]] = 0;
+        fields[list[idxFd]] += 1;
+        if (!students[list[idxFd]]) students[list[idxFd]] = '';
+        students[list[idxFd]] += students[list[idxFd]]
+          ? `, ${list[idxFn]}`
+          : list[idxFn];
+      });
+
+      all.numberStudents = `Number of students: ${lines.length}\n`;
+      all.listStudents = [];
+      for (const key in fields) {
+        if (Object.hasOwnProperty.call(fields, key)) {
+          const element = fields[key];
+          all.listStudents.push(`Number of students in ${key}: ${element}. List: ${students[key]}`);
+        }
+      }
+      return resolve(all);
+    });
+  });
+}
 
 const app = express();
-const PORT = 1245;
-
-app.use((req, res, next) => {
-  const dbFilePath = process.argv[2];
-  if (!dbFilePath) {
-    return res.status(500).send('No database file specified');
-  }
-  next();
-});
+const port = 1245;
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
-
-app.get('/students', async (req, res) => {
-  const dbFilePath = process.argv[2];
-  try {
-    const result = await countStudents(dbFilePath);
-    res.status(200).send(`This is the list of our students\n${result}`);
-  } catch (error) {
-    res.status(404).send(error.message);
-  }
+app.get('/students', (req, res) => {
+  res.write('This is the list of our students\n');
+  countStudents(process.argv[2])
+    .then((data) => {
+      res.write(data.numberStudents);
+      res.end(data.listStudents.join('\n'));
+    })
+    .catch((err) => {
+      res.end(err.message);
+    });
 });
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+app.listen(port);
 
 module.exports = app;
